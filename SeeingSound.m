8b1cb1386daebb49ccdec65664c58425a99a24f0
MiladKhez <168370783+MiladKhez@users.noreply.github.com>
2026-04-17 20:13:55 -0500

    Add files via upload

---
+++ SeeingSound.m
@@ -0,0 +1,7 @@
+%======================================
+%Audio Filter and Visualizer:
+%This program takes live audio as an input and distinctly filters and
+%separates different frequencies into matricies. The program then produces
+%a live plot of the data as frequency vs in an x and y component.The
+%program also includes a feature that allows the user to have the program
+%stop when a certain sound intensity is met.
