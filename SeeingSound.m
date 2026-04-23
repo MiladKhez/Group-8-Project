b224b8c234820704036c22dab8f60bdf880ad4f4
MiladKhez <168370783+MiladKhez@users.noreply.github.com>
2026-04-22 22:30:41 -0500

    Update SeeingSound.m
    This has the added features of prompting the user to play a SONG or use live listening to the audio AROUND them

--- SeeingSound.m
+++ SeeingSound.m
@@ -6,29 +6,85 @@
 %program also includes a feature that allows the user to have the program
 %stop when a certain sound intensity is met.
 
-%Establish audion input port
-deviceReader = audioDeviceReader('SampleRate',44100,'NumChannels',2, 'SamplesPerFrame',1024);
+
+
+% fileReader = dsp.AudioFileReader('Replay_I_Y_A_Z_Naijapals.mp3');%
+% deviceWriter = AudioFileWriter('SampleRate',fileReader.SampleRate);%
+% 
+% crossFilt = crossoverFilter('SampleRate',fileReader.SampleRate)
+% visualize(crossFilt);
+% parameterTuner(crossFilt);
+
+% %Establish audion input port **NOTE: Computer must have access to both (2) stereo
+%channels to run program
+clear all; clc;
+
+deviceReader = audioDeviceReader('SampleRate',44100,'NumChannels',2, 'SamplesPerFrame',1024);%establishes the computer mic as the input reader
+
+[y,fs] = audioread('Replay_I_Y_A_Z_Naijapals.mp3');
+player = audioplayer(y, fs);
+
+
+Void = nan;
+
+V = 'void';
+while V ~= "AROUND" || V~= "SONG"
+    fprintf('ERROR: Please Enter "Around" OR "Song"\n')
+    V = input('Enter: "Around" OR "Song"\n', 's');
+    V = upper(V);
+    disp(V);
+if V == "AROUND" || V == "SONG"
+    break
+end
+end
+
+if V == "SONG"
+
+play(player);
+
 
 figure
-h = plot(nan, nan, '.')%create plot 
-axis([-1 1 -1 1])
+h = plot(Void, Void, '.');%create plot 
+axis([-1 1 -1 1]);%keep range/domain small when using computer input mic (also when normalize is off)
 xlabel('left channel')
 ylabel('right channel')
 title('See The Sound')
 
-ax = gca;
-ax.XLimMode = 'manual';
-ax.YLimMode = 'manual';
+while true
+    % fr = matlab.io.datastore.DsFileReader('Replay_I_Y_A_Z_Naijapals.mp3')
+    audioFrame = player()
+    % deviceWriter(audioFrame);
+
+    %Normalize??
+    % audioFrame = audsioFrame/ max(abs(audioFrame(:)) + 1e-6)
+
+    set(h, 'Xdata', audioFrame(:,1), 'Ydata', audioFrame(:,2));
+
+    drawnow limitrate
+end
+end
+
+% ax = gca;
+% ax.XLimMode = 'manual';
+% ax.YLimMode = 'manual';
+
+if V == "AROUND"
+
+figure
+h = plot(Void, Void, '.');%create plot 
+axis([-.1 .1 -.1 .1]);%keep range/domain small when using computer input mic (also when normalize is off)
+xlabel('left channel')
+ylabel('right channel')
+title('See The Sound')
 
 while true
     audioFrame = deviceReader()
 
     %Normalize??
-    audioFrame = audioFrame/ max(abs(audioFrame(:)) + 1e-6)
+    % audioFrame = audioFrame/ max(abs(audioFrame(:)) + 1e-6)
 
     set(h, 'Xdata', audioFrame(:,1), 'Ydata', audioFrame(:,2));
 
     drawnow limitrate
-
 end
-
+end
